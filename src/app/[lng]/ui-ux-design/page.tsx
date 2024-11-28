6c42e5768c43d9f0c52091aea58a6209a2d7d496
AlexP <86realmadrid86@gmail.com>
2024-11-28 18:30:35 +0200

    feat: add ui/ux design page

---
+++ src/app/[lng]/ui-ux-design/page.tsx
@@ -0,0 +1,40 @@
+import { PROJECTS_QUERY } from '@graphqlQueries/ProjectsQuery';
+import { fetchGraphQL } from '@lib/fetchGraphQL';
+
+import { Accordion } from '@components/Accordion';
+import { Clients } from '@components/Clients';
+import { ContactForm } from '@components/ContactForm';
+import { CoreServices } from '@components/CoreServices';
+import { Hero } from '@components/Hero';
+import { OurWork } from '@components/OurWork';
+import { TechnologyStack } from '@components/TechnologyStack';
+
+import bottomBgImage from './img/bg.png';
+import heroImg from './img/heroImg.png';
+import technologyImg from './img/technologyImg.png';
+
+export default async function UiUxDesignPage() {
+  const { singleProjects } = await fetchGraphQL(PROJECTS_QUERY, {
+    locale: 'en',
+    pagination: { limit: 5 },
+  });
+
+  const singleProjectsData = singleProjects?.data || [];
+
+  return (
+    <>
+      <Hero
+        page="uiUxDesigne"
+        image={heroImg}
+        bottomBgImage={bottomBgImage}
+        colorGradiant="Third"
+      />
+      <TechnologyStack image={technologyImg} page="uiUxDesigne" />
+      <CoreServices page="uiUxDesigne" />
+      <Clients />
+      <OurWork data={singleProjectsData} />
+      <Accordion />
+      <ContactForm />
+    </>
+  );
+}
