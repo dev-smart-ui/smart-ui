b764b63d74723dc8cfdabca9d8244bb549fe1221
AlexP <86realmadrid86@gmail.com>
2024-12-06 17:39:53 +0200

    feat(forAgency): add advantages section

--- src/app/[lng]/for-agency/components/Advantages/Advantages.tsx
+++ src/app/[lng]/for-agency/components/Advantages/Advantages.tsx
@@ -4,28 +4,40 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Container } from '@components/Container';
+import { InfoCards } from '@components/InfoCards';
 import { Section } from '@components/Section';
 import { SectionHeader } from '@components/SectionHeader';
 
+import { Solutions } from './Solutions';
 import styles from './advantages.module.scss';
 
+const infoCards = [
+  { count: '250+', label: 'infoCards.card1.label' },
+  { count: '117+', label: 'infoCards.card2.label' },
+];
+
 export const Advantages: FC = () => {
-  const { t } = useTranslation('forAgency');
+  const { t } = useTranslation(['forAgency', 'common']);
 
   const headerInfo = {
-    sectionName: t('advantages.sectionName'),
-    title: t('advantages.title'),
-    subTitle: t('advantages.description'),
+    sectionName: t('advantages.headerInfo.sectionName'),
+    title: t('advantages.headerInfo.title'),
+    subTitle: t('advantages.headerInfo.description'),
   };
 
   return (
     <Section>
       <Container className={styles.content}>
-        <SectionHeader
-          sectionName={headerInfo.sectionName}
-          title={headerInfo.title}
-          subTitle={headerInfo.subTitle}
-        />
+        <div className={styles.advantages}>
+          <SectionHeader
+            className={styles.sectionHeader}
+            sectionName={headerInfo.sectionName}
+            title={headerInfo.title}
+            subTitle={headerInfo.subTitle}
+          />
+          <InfoCards t={t} data={infoCards} className={styles.infoCards} />
+        </div>
+        <Solutions t={t} />
       </Container>
     </Section>
   );
